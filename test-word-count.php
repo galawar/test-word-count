0cb6a889cf70cb5e2b1aedb3f8ae6e1f1b5199ff
galawar <galawarz@gmail.com>
2023-07-15 14:38:43 +0300

    init TestWordCount class

--- test-word-count.php
+++ test-word-count.php
@@ -6,3 +6,32 @@
  Author: galawar
  Authir URI:
 */
+
+class TestWordCount {
+	public function __construct() {
+		add_action(
+			'admin_menu',
+			array( $this, 'adminPage' )
+		);
+	}
+
+	public function adminPage() {
+		add_options_page(
+			'Word Count Settings',
+			'Word Count',
+			'manage_options',
+			'word-count-settings-page',
+			array( $this, 'pageHTML' ),
+		);
+	}
+
+	public function pageHTML() {
+		?>
+		<div class="wrap">
+			<h1>Word Count settings.</h1>
+		</div>
+		<?php
+	}
+}
+
+$wordCountPlugin = new TestWordCount();
