3be50e10c25eefc0ceb189c574c9e367643a8390
galawar <galawarz@gmail.com>
2023-07-15 16:25:24 +0300

    selected state for option

--- test-word-count.php
+++ test-word-count.php
@@ -53,8 +53,8 @@ class TestWordCount {
 	public function locationHTML() {
 		?>
 		<select name="wcp_location">
-			<option value="0">Beginning of post</option>
-			<option value="1">End of post</option>
+			<option value="0" <?php selected( get_option( 'wcp_location' ), '0' ); ?> >Beginning of post</option>
+			<option value="1" <?php selected( get_option( 'wcp_location' ), '1' ); ?> >End of post</option>
 		</select>
 		<?php
 	}
